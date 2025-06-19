5d1d594d75d97826102088c90e6ee59cba69eb12
razanz <zbibrazan147@gmail.com>
2025-06-19 21:54:19 +0300

    add header and player info section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-
+import Player from "./components/Player";
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialPlayerName="Player 1" playerSymbol="X" />
+          <Player initialPlayerName="Player 2" playerSymbol="O" />
+        </ol>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Player.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export default function Player({ initialPlayerName, playerSymbol }) {
+  const [playerName, setPlayerName] = useState(initialPlayerName);
+  const [isEditing, setIsEditing] = useState(false);
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+  function handleNameChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" value={playerName} onChange={handleNameChange} />
+    );
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{playerSymbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
